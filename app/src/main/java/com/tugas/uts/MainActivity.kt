017b89e48ba02466213a87db89e59b31e7d426b5
Wildan <wdesnandar@gmail.com>
2022-10-24 22:10:49 +0700

    Tugas UTS - Mini Kalkulator

--- app/src/main/java/com/tugas/uts/MainActivity.kt
+++ app/src/main/java/com/tugas/uts/MainActivity.kt
@@ -13,13 +13,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val buttonTambah: Button = findViewById(R.id.btnTambah)
+        val buttonKurang: Button = findViewById(R.id.btnKurang)
         val buttonKali: Button = findViewById(R.id.btnKali)
+        val buttonBagi: Button = findViewById(R.id.btnBagi)
 
         buttonTambah.setOnClickListener() {
             penjumlahanAngka()
         }
+        buttonKurang.setOnClickListener() {
+            penguranganAngka()
+        }
         buttonKali.setOnClickListener() {
             perkalianAngka()
+        }
+        buttonBagi.setOnClickListener() {
+            pembagianAngka()
         }
     }
     private fun penjumlahanAngka() {
@@ -44,6 +52,28 @@ class MainActivity : AppCompatActivity() {
             txtResult.text = result.toString()
         }
     }
+    private fun penguranganAngka() {
+        val txtInput1: EditText = findViewById(R.id.txtInput1)
+        val txtInput2: EditText = findViewById(R.id.txtInput2)
+        val txtResult: TextView = findViewById(R.id.txtViewResult)
+
+        val input1 = txtInput1.text.toString().trim()
+        val input2 = txtInput2.text.toString().trim()
+
+        var isEmptyField = false
+        if (input1.isEmpty()) {
+            isEmptyField = true;
+            txtInput1.error = "Input 1 harus diisi angka (0-9)"
+        }
+        if (input2.isEmpty()) {
+            isEmptyField = true
+            txtInput2.error = "Input 2 harus diisi angka (0-9)"
+        }
+        if (!isEmptyField) {
+            val result = input1.toInt() - input2.toInt()
+            txtResult.text = result.toString()
+        }
+    }
     private fun perkalianAngka() {
         val txtInput1: EditText = findViewById(R.id.txtInput1)
         val txtInput2: EditText = findViewById(R.id.txtInput2)
@@ -63,6 +93,28 @@ class MainActivity : AppCompatActivity() {
         }
         if (!isEmptyField) {
             val result = input1.toInt() * input2.toInt()
+            txtResult.text = result.toString()
+        }
+    }
+    private fun pembagianAngka() {
+        val txtInput1: EditText = findViewById(R.id.txtInput1)
+        val txtInput2: EditText = findViewById(R.id.txtInput2)
+        val txtResult: TextView = findViewById(R.id.txtViewResult)
+
+        val input1 = txtInput1.text.toString().trim()
+        val input2 = txtInput2.text.toString().trim()
+
+        var isEmptyField = false
+        if (input1.isEmpty()) {
+            isEmptyField = true;
+            txtInput1.error = "Input 1 harus diisi angka (0-9)"
+        }
+        if (input2.isEmpty()) {
+            isEmptyField = true
+            txtInput2.error = "Input 2 harus diisi angka (0-9)"
+        }
+        if (!isEmptyField) {
+            val result = input1.toInt() / input2.toInt()
             txtResult.text = result.toString()
         }
     }
